c8b5ab552e76d96ca12b1d4cce4ee348c279196b
VigneshD <VigneshD@CESIT-LAP-0135>
2021-09-24 02:50:36 +0530

    pom updated

--- src/main/java/commonUtil/ComUtil.java
+++ src/main/java/commonUtil/ComUtil.java
@@ -2,16 +2,31 @@ package commonUtil;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+
+import java.io.File;
+
 import org.testng.Reporter;
 
 public class ComUtil {
-
+	protected File classPathRoot = new File(System.getProperty("user.dir"));
+	protected File resourcesRoot = new File(classPathRoot,"src/test/resources");
 	protected AppiumDriver<MobileElement> driver;
 	public void log(String LogMessage) {
 		Reporter.log(LogMessage);
 		System.out.println(LogMessage);
 	}
 	
+	public String getAppPath(String AppName){
+		File app= new File(resourcesRoot,"/APK/"+AppName);
+		return app.getAbsolutePath();
+
+	}
+	
+	public String getExcelPath(String ExcelName){
+		File app= new File(resourcesRoot,"/ExcelData/"+ExcelName);
+		return app.getAbsolutePath();
+
+	}
 	
 
 }
